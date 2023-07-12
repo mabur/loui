eab4bf228314020ce4fe5efb3204938f6a8b78a0
Magnus Burenius <magnus.burenius@gmail.com>
2023-07-12 23:17:05 +0200

    refactor: remove scalar color constants

--- src/color.hpp
+++ src/color.hpp
@@ -6,19 +6,6 @@ using Color = uint32_t;
 
 Color packColorRgb(uint32_t r, uint32_t g, uint32_t b);
 
-const auto BLACK = packColorRgb(0, 0, 0);
-const auto BULLET_PLAYER = packColorRgb(32, 32, 32);
-const auto BULLET_ENEMY = packColorRgb(0, 32, 0);
-const auto WHITE = packColorRgb(255, 255, 255);
-const auto GRAY = packColorRgb(64, 64, 64);
-const auto PINK = packColorRgb(255, 129, 185);
-const auto RED = packColorRgb(255, 0, 0);
-const auto BLUE = packColorRgb(0, 0, 255);
-const auto BLOOD = packColorRgb(128, 0, 0);
-const auto GREEN = packColorRgb(0, 255, 0);
-const auto GOLD = packColorRgb(246, 183, 71);
-const auto LAMP = packColorRgb(140, 220, 255);
-
 struct ColorShades {
     Color foreground;
     Color background;
