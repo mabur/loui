3934d1953bb355daadf9e57817f8bedb9cef082f
Magnus Burenius <magnus.burenius@gmail.com>
2025-01-12 18:46:36 +0100

    add selection box to example

--- example_sdl/main.cpp
+++ example_sdl/main.cpp
@@ -22,18 +22,33 @@ int main(int, char**) {
         GUI90_WidgetBackground(GUI90_THEME_LEATHER);
         auto x = 16;
         auto y = 16;
-        if (GUI90_WidgetLabel(x, y, "Play", GUI90_THEME_LEATHER)) {
+        if (GUI90_WidgetLabel(x, y, "Label", GUI90_THEME_LEATHER)) {
         }
         y += 16;
-        if (GUI90_WidgetButton(x, y, "Build", GUI90_THEME_GRAY)) {
-        }
-        y += 32;
-        if (GUI90_WidgetButton(x, y, "Exit", GUI90_THEME_GRAY)) {
-            break;
+        if (GUI90_WidgetButton(x, y, "Button", GUI90_THEME_GRAY)) {
         }
         y += 16;
         static int setting = 0;
         setting = GUI90_WidgetIntSetting(x, y, "Setting", setting, 0, 10, GUI90_THEME_LEATHER, GUI90_THEME_GRAY);
+        y += 32;
+        static auto selection = 0;
+        static const auto N = 4;
+        const char* options[N] = {
+            "Selection A",
+            "Selection B",
+            "Selection C",
+            "Selection D",
+        };
+        auto width = 8 * 13;
+        auto height = 8 * (N + 2);
+        GUI90_WidgetSelectionBoxInit(x, y, width, height, GUI90_THEME_LEATHER);
+        for (auto i = 0; i < N; ++i) {
+            if (GUI90_WidgetSelectionBoxItem(options[i], selection == i)) {
+                selection = i;
+            }
+        }
+        y += height;
+        
         sdl.draw(GUI90_GetPixelData());
     }
     return 0;
