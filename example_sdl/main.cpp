cb41788f6370774012a2130e5cbe1a1f2b1c0613
Magnus Burenius <magnus.burenius@gmail.com>
2025-01-18 12:15:29 +0100

    interactive gui theme selection

--- example_sdl/main.cpp
+++ example_sdl/main.cpp
@@ -1,8 +1,38 @@
 #define SDL_MAIN_HANDLED
 
+#include <assert.h>
+
 #include <gui90/gui.hpp>
 
 #include "sdl_wrappers.hpp"
+
+enum GuiThemeIndex {
+    YELLOW_THEME_INDEX,
+    GRAY_THEME_INDEX,
+    LEATHER_THEME_INDEX,
+    SOLARIZE_LIGHT_THEME_INDEX,
+    THEME_COUNT,
+};
+
+GUI90_Theme themeSettings(GuiThemeIndex theme) {
+    switch (theme) {
+        case YELLOW_THEME_INDEX: return GUI90_THEME_YELLOW;
+        case GRAY_THEME_INDEX: return GUI90_THEME_GRAY;
+        case LEATHER_THEME_INDEX: return GUI90_THEME_LEATHER;
+        case SOLARIZE_LIGHT_THEME_INDEX: return GUI90_THEME_SOLARIZE_LIGHT;
+    }
+    assert(false and "Unknown theme");
+}
+
+const char* themeDescription(GuiThemeIndex theme) {
+    switch (theme) {
+        case YELLOW_THEME_INDEX: return "Yellow";
+        case GRAY_THEME_INDEX: return "Gray";
+        case LEATHER_THEME_INDEX: return "Leather";
+        case SOLARIZE_LIGHT_THEME_INDEX: return "Solarize";
+    }
+    assert(false and "Unknown theme");
+}
 
 int main(int, char**) {
     const auto WINDOW_TITLE = "GUI90";
@@ -19,8 +49,10 @@ int main(int, char**) {
             break;
         }
         GUI90_SetMouseState(input.mouse_x, input.mouse_y, input.isLeftMouseButtonDown());
+
+        static auto theme_index = YELLOW_THEME_INDEX;
+        auto theme = themeSettings(theme_index);
         
-        auto theme = GUI90_THEME_YELLOW;
         GUI90_WidgetBackground(theme);
         auto x = 16;
         auto y = 16;
@@ -33,20 +65,16 @@ int main(int, char**) {
         static int setting = 0;
         setting = GUI90_WidgetIntSetting(x, y, "Setting", setting, 0, 10, theme);
         y += 32;
-        static auto selection = 0;
-        static const auto N = 4;
-        const char* options[N] = {
-            "Selection A",
-            "Selection B",
-            "Selection C",
-            "Selection D",
-        };
+
         auto width = 8 * 13;
-        auto height = 8 * (N + 2);
+        auto height = 8 * (THEME_COUNT + 2);
+        
         GUI90_WidgetSelectionBoxInit(x, y, width, height, theme);
-        for (auto i = 0; i < N; ++i) {
-            if (GUI90_WidgetSelectionBoxItem(options[i], selection == i)) {
-                selection = i;
+        for (auto i = 0; i < THEME_COUNT; ++i) {
+            auto local_theme_index = (GuiThemeIndex)i;
+            auto theme_description = themeDescription(local_theme_index);
+            if (GUI90_WidgetSelectionBoxItem(theme_description, theme_index == local_theme_index)) {
+                theme_index = local_theme_index;
             }
         }
         y += height;
