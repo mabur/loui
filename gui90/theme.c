97bd14f184a0ad820bde0d3ab4a36868d9d95186
Magnus Burenius <magnus.burenius@gmail.com>
2025-04-12 14:30:31 +0200

    update solarize theme

--- gui90/theme.c
+++ gui90/theme.c
@@ -65,14 +65,14 @@ const LouiTheme LOUI_THEME_LEATHER = (LouiTheme){
 };
 
 const LouiTheme LOUI_THEME_SOLARIZE_LIGHT = (LouiTheme){
-    .background = LOUI_RGB(253, 246, 227),
-    .text = LOUI_RGB(0, 0, 0),
-    .button_background = LOUI_RGB(255, 255, 255),
-    .button_text = LOUI_RGB(0, 0, 0),
-    .button_border = LOUI_RGB(0, 0, 0),
-    .button_bevel_dark = LOUI_RGB(192, 192, 192),
-    .button_bevel_light = LOUI_RGB(255, 255, 255),
-    .recess_background = LOUI_RGB(0, 0, 0),
+    .background = LOUI_RGB(238, 232, 213),
+    .text = LOUI_RGB(39, 40, 34), // LOUI_RGB(0, 0, 0),
+    .button_background =  LOUI_RGB(238, 232, 213), // LOUI_RGB(255, 255, 255),
+    .button_text = LOUI_RGB(39, 40, 34), // LOUI_RGB(0, 0, 0),
+    .button_border = LOUI_RGB(39, 40, 34), // LOUI_RGB(0, 0, 0),
+    .button_bevel_dark = LOUI_RGB(134, 116, 96), // LOUI_RGB(192, 192, 192),
+    .button_bevel_light = LOUI_RGB(255, 255, 255), // LOUI_RGB(255, 255, 255),
+    .recess_background = LOUI_RGB(39, 40, 34), // LOUI_RGB(0, 0, 0),
     .recess_text = LOUI_RGB(128, 128, 128),
     .recess_text_selected = LOUI_RGB(255, 255, 255),
     .recess_bevel_dark = LOUI_RGB(134, 116, 96),
