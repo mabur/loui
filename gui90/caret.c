0e5224746c36f6ec9403e8c084459fd97a167658
Magnus Burenius <magnus.burenius@gmail.com>
2025-04-18 16:23:58 +0200

    refactor: move string.c inside caret.c

--- gui90/caret.c
+++ gui90/caret.c
@@ -2,7 +2,51 @@
 
 #include <string.h>
 
-#include "string.h"
+static int countLines(const char* text) {
+    auto lines = 1;
+    for (; *text; ++text) {
+        if (*text == '\n') {
+            ++lines;
+        }
+    }
+    return lines;
+}
+
+static const char* gotoEndOfLine(const char* text) {
+    for (;*text != '\0' || *text != '\n'; ++text) {
+    }
+    return text;
+}
+
+static const char* gotoNextLine(const char* text) {
+    for (;; ++text) {
+        if (*text == '\0') {
+            return text;
+        }
+        if (*text == '\n') {
+            ++text;
+            return text;
+        }
+    }
+}
+
+static const char* gotoRow(const char* text, int row) {
+    for (;row > 0; --row) {
+        text = gotoNextLine(text);
+    }
+    return text;
+}
+
+static int countColumnsOfFirstRow(const char* text) {
+    auto columns = 0;
+    for (; *text && *text != '\n'; ++text, ++columns) {
+    }
+    return columns;
+}
+
+static int countColumns(const char* text, int row) {
+    return countColumnsOfFirstRow(gotoRow(text, row));
+}
 
 SingleLineCaret moveSingleLineCaretColumn(SingleLineCaret caret, const char* text, int column) {
     auto count = (int)strlen(text);
--- gui90/gui.c
+++ gui90/gui.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "string.h"
 #include "text.h"
 
 #define DRAW_DEBUG_RECTANGLES 0
