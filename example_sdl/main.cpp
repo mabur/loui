4b1eabc70d7b308cb9696d3316046d7e9e73de4d
Magnus Burenius <magnus.burenius@gmail.com>
2025-05-30 17:35:59 +0200

    refactor: extract update function

--- example_sdl/main.cpp
+++ example_sdl/main.cpp
@@ -51,6 +51,159 @@ LouiInput createLouiInput(Input input, char input_character, int mouse_wheel_y) 
     return loui_input;
 }
 
+bool update(Sdl& sdl, int WIDTH, int HEIGHT) {
+    auto event = SDL_Event();
+    char input_character = '\0';
+    auto mouse_wheel_y = 0;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+        if (event.type == SDL_TEXTINPUT) {
+            input_character = event.text.text[0];
+        }
+        if (event.type == SDL_MOUSEWHEEL) {
+            mouse_wheel_y = event.wheel.y;
+        }
+    }
+    auto sdl_input = sdl.getInput();
+    if (sdl_input.escape_button == BUTTON_CLICKED) {
+        return false;
+    }
+    auto loui_input = createLouiInput(sdl_input, input_character, mouse_wheel_y);
+    loui_set_input(loui_input);
+
+    static auto theme_index = YELLOW_THEME_INDEX;
+    static auto button_type = BUTTON_TYPE_BEVEL;
+    auto theme = themeSettings[theme_index];
+    theme.button_type = button_type;
+    loui_set_theme(theme);
+
+    loui_widget_background();
+    auto x = 2 * LOUI_BLOCK;
+    auto y = 2 * LOUI_BLOCK;
+    auto label = (LouiLabel){.x=x, .y=y, .text="Label"};
+    loui_update(label);
+    y += label.height;
+    y += LOUI_BLOCK;
+    auto open_button = (LouiButton){.x=x, .y=y, .text="Open "};
+    loui_update(open_button);
+    if (open_button.is_clicked) {
+    }
+    y += open_button.height;
+    auto close_button = (LouiButton){.x=x, .y=y, .text="Close"};
+    loui_update(close_button);
+    if (close_button.is_clicked) {
+    }
+    y += close_button.height;
+
+    static int setting = 0;
+    char setting_text[64];
+    sprintf(setting_text, "Setting: %d ", setting);
+    auto stepper = (LouiStepper){.x=x, .y=y, .text=setting_text};
+    loui_update(stepper);
+    if (stepper.is_decreased and setting > 0) {
+        setting--;
+    }
+    if (stepper.is_increased and setting < 10) {
+        setting++;
+    }
+    y += stepper.height;
+    y += LOUI_BLOCK;
+
+    auto width = 13 * LOUI_BLOCK;
+    auto height = (THEME_COUNT + 2) * LOUI_BLOCK;
+    auto selection_box = (LouiSelectionBoxInit){.x=x, .y=y, .width=width, .height=height};
+    loui_update(selection_box);
+    for (auto i = 0; i < THEME_COUNT; ++i) {
+        auto local_theme_index = (GuiThemeIndex)i;
+        auto theme_description = themeDescription[local_theme_index];
+        auto item = (LouiSelectionBoxItem){
+            .text=theme_description, .is_selected=theme_index == local_theme_index
+        };
+        loui_update(item);
+        if (item.is_clicked) {
+            theme_index = local_theme_index;
+        }
+    }
+    y += selection_box.height;
+
+    auto radio_button_a = (LouiRadioButton){
+        .x=x, .y=y, .text="Bevel Buttons", .is_selected=button_type == BUTTON_TYPE_BEVEL
+    };
+    loui_update(radio_button_a);
+    if (radio_button_a.is_clicked) {
+        button_type = BUTTON_TYPE_BEVEL;
+    }
+    y += radio_button_a.height;
+    auto radio_button_b = (LouiRadioButton) {
+        .x=x, .y=y, .text="Cloud Buttons", .is_selected=button_type == BUTTON_TYPE_CLOUD
+    };
+    loui_update(radio_button_b);
+    if (radio_button_b.is_clicked) {
+        button_type = BUTTON_TYPE_CLOUD;
+    }
+
+    // Second Column
+    x = WIDTH / 2;
+    y = 2 * LOUI_BLOCK;
+
+    auto WHITE = LOUI_RGB(255, 255, 255);
+    auto BLACK = LOUI_RGB(0, 0, 0);
+    auto MID = loui_interpolate_colors(BLACK, theme.background, 128);
+    auto header_theme = LouiHeaderLabelTheme{
+        .color_up_left = MID,
+        .color_up = BLACK,
+        .color_up_right = MID,
+        .color_left = BLACK,
+        .color_center = WHITE,
+        .color_right = BLACK,
+        .color_down_left = MID,
+        .color_down = BLACK,
+        .color_down_right = MID,
+        .draw_up_left = true,
+        .draw_up = true,
+        .draw_up_right = true,
+        .draw_left = true,
+        .draw_center = true,
+        .draw_right = true,
+        .draw_down_left = true,
+        .draw_down = true,
+        .draw_down_right = true,
+    };
+    auto header = (LouiHeaderLabel){.x=x, .y=y, .text="Header LABEL", .theme=header_theme};
+    loui_update(header);
+
+    y += header.height + LOUI_BLOCK;
+    static auto input0 = (LouiTextInput){.x=x, .y=y, .text="Input 0"};
+    loui_update(input0);
+    y += input0.height;
+    static auto input1 = (LouiTextInput){.x=x, .y=y, .text="Input 1"};
+    loui_update(input1);
+    y += input1.height;
+    y += LOUI_BLOCK;
+
+    static auto multi_text_input = (LouiMultiTextInput){
+        .x=x,
+        .y=y,
+        .text="Multi\nlines of\ntext\na very very long line\nb\nc\nd\ne\nf",
+        .lines=7,
+        .columns=14,
+        .draw_caret=(MultiLineCaret){.line=0, .column=0},
+    };
+    loui_update(multi_text_input);
+    y += multi_text_input.height;
+
+    static auto check_box0 = (LouiCheckBox){.x=x, .y=y, .text="Check Box 0", .is_selected=true};
+    loui_update(check_box0);
+    y += check_box0.height;
+    static auto check_box1 = (LouiCheckBox){.x=x, .y=y, .text="Check Box 1"};
+    loui_update(check_box1);
+
+    sdl.draw(loui_get_pixel_data());
+    return true;
+}
+
 int main() {
     auto WINDOW_TITLE = "Loui";
     auto WIDTH = 320;
@@ -61,155 +214,9 @@ int main() {
     sdl.setMouseModeAbsolute();
     
     for (;;) {
-        auto event = SDL_Event();
-        char input_character = '\0';
-        auto mouse_wheel_y = 0;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                break;
-            }
-            if (event.type == SDL_TEXTINPUT) {
-                input_character = event.text.text[0];
-            }
-            if (event.type == SDL_MOUSEWHEEL) {
-                mouse_wheel_y = event.wheel.y;
-            }
-        }
-        auto sdl_input = sdl.getInput();
-        if (sdl_input.escape_button == BUTTON_CLICKED) {
-            break;
-        }
-        auto loui_input = createLouiInput(sdl_input, input_character, mouse_wheel_y);
-        loui_set_input(loui_input);
-        
-        static auto theme_index = YELLOW_THEME_INDEX;
-        static auto button_type = BUTTON_TYPE_BEVEL;
-        auto theme = themeSettings[theme_index];
-        theme.button_type = button_type;
-        loui_set_theme(theme);
-        
-        loui_widget_background();
-        auto x = 2 * LOUI_BLOCK;
-        auto y = 2 * LOUI_BLOCK;
-        auto label = (LouiLabel){.x=x, .y=y, .text="Label"};
-        loui_update(label);
-        y += label.height;
-        y += LOUI_BLOCK;
-        auto open_button = (LouiButton){.x=x, .y=y, .text="Open "};
-        loui_update(open_button);
-        if (open_button.is_clicked) {
-        }
-        y += open_button.height;
-        auto close_button = (LouiButton){.x=x, .y=y, .text="Close"};
-        loui_update(close_button);
-        if (close_button.is_clicked) {
-        }
-        y += close_button.height;
-
-        static int setting = 0;
-        char setting_text[64];
-        sprintf(setting_text, "Setting: %d ", setting);
-        auto stepper = (LouiStepper){.x=x, .y=y, .text=setting_text};
-        loui_update(stepper);
-        if (stepper.is_decreased and setting > 0) {
-            setting--;
-        }
-        if (stepper.is_increased and setting < 10) {
-            setting++;
-        }
-        y += stepper.height;
-        y += LOUI_BLOCK;
-
-        auto width = 13 * LOUI_BLOCK;
-        auto height = (THEME_COUNT + 2) * LOUI_BLOCK;
-        auto selection_box = (LouiSelectionBoxInit){.x=x, .y=y, .width=width, .height=height};
-        loui_update(selection_box);
-        for (auto i = 0; i < THEME_COUNT; ++i) {
-            auto local_theme_index = (GuiThemeIndex)i;
-            auto theme_description = themeDescription[local_theme_index];
-            auto item = (LouiSelectionBoxItem){
-                .text=theme_description, .is_selected=theme_index == local_theme_index
-            };
-            loui_update(item);
-            if (item.is_clicked) {
-                theme_index = local_theme_index;
-            }
-        }
-        y += selection_box.height;
-
-        auto radio_button_a = (LouiRadioButton){
-            .x=x, .y=y, .text="Bevel Buttons", .is_selected=button_type == BUTTON_TYPE_BEVEL
-        };
-        loui_update(radio_button_a);
-        if (radio_button_a.is_clicked) {
-            button_type = BUTTON_TYPE_BEVEL;
-        }
-        y += radio_button_a.height;
-        auto radio_button_b = (LouiRadioButton) {
-            .x=x, .y=y, .text="Cloud Buttons", .is_selected=button_type == BUTTON_TYPE_CLOUD
-        };
-        loui_update(radio_button_b);
-        if (radio_button_b.is_clicked) {
-            button_type = BUTTON_TYPE_CLOUD;
-        }
-
-        // Second Column
-        x = WIDTH / 2;
-        y = 2 * LOUI_BLOCK;
-        
-        auto WHITE = LOUI_RGB(255, 255, 255);
-        auto BLACK = LOUI_RGB(0, 0, 0);
-        auto MID = loui_interpolate_colors(BLACK, theme.background, 128);
-        auto header_theme = LouiHeaderLabelTheme{
-            .color_up_left = MID,
-            .color_up = BLACK,
-            .color_up_right = MID,
-            .color_left = BLACK,
-            .color_center = WHITE,
-            .color_right = BLACK,
-            .color_down_left = MID,
-            .color_down = BLACK,
-            .color_down_right = MID,
-            .draw_up_left = true,
-            .draw_up = true,
-            .draw_up_right = true,
-            .draw_left = true,
-            .draw_center = true,
-            .draw_right = true,
-            .draw_down_left = true,
-            .draw_down = true,
-            .draw_down_right = true,
-        };
-        auto header = (LouiHeaderLabel){.x=x, .y=y, .text="Header LABEL", .theme=header_theme};
-        loui_update(header);
-
-        y += header.height + LOUI_BLOCK;
-        static auto input0 = (LouiTextInput){.x=x, .y=y, .text="Input 0"};
-        loui_update(input0);
-        y += input0.height;
-        static auto input1 = (LouiTextInput){.x=x, .y=y, .text="Input 1"};
-        loui_update(input1);
-        y += input1.height;
-        y += LOUI_BLOCK;
-
-        static auto multi_text_input = (LouiMultiTextInput){
-            .x=x,
-            .y=y,
-            .text="Multi\nlines of\ntext\na very very long line\nb\nc\nd\ne\nf",
-            .lines=7,
-            .columns=14,
-            .draw_caret=(MultiLineCaret){.line=0, .column=0},
-        };
-        loui_update(multi_text_input);
-        y += multi_text_input.height;
-
-        static auto check_box0 = (LouiCheckBox){.x=x, .y=y, .text="Check Box 0", .is_selected=true};
-        loui_update(check_box0);
-        y += check_box0.height;
-        static auto check_box1 = (LouiCheckBox){.x=x, .y=y, .text="Check Box 1"};
-        loui_update(check_box1);
-
-        sdl.draw(loui_get_pixel_data());
+       if (!update(sdl, WIDTH, HEIGHT)) {
+           break;
+       }
     }
     return 0;
 }
