d144e5242f13b5ed1b60c097de4ed79cc755ff1f
Magnus Burenius <magnus.burenius@gmail.com>
2026-04-12 19:39:24 +0200

    change separator color

--- loui/loui_separator.c
+++ loui/loui_separator.c
@@ -7,7 +7,7 @@
 LouiSeparator loui_update_separator(LouiSeparator widget) {
     widget.height = 8;
     for (auto x = widget.x; x < widget.x + widget.width; x += 2) {
-        drawPoint(s_loui.screen, x, widget.y + 3, getTheme().button_bevel_dark);    
+        drawPoint(s_loui.screen, x, widget.y + 3, getTheme().recess_bevel_dark);    
     }
     return widget;
 }
